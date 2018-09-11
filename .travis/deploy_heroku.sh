1c5a6560d23d538813b2224f29d96807aac82c61
aygunaydin <aygunaydin@gmail.com>
2018-09-11 09:44:59 +0200

    heroku stack fix

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -2,5 +2,6 @@
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 heroku plugins:install heroku-container-registry
 docker login -u aygunaydin@gmail.com --password=$HEROKU_API_KEY registry.heroku.com
+heroku stack:set heroku-16
 heroku container:release web --app $HEROKU_APP_NAME
 
