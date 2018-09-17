b76b349ed3af7ee8e89255fc6faacf156969358e
aygunaydin <aygunaydin@gmail.com>
2018-09-17 10:00:17 +0200

    heroku version is updated

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+heroku stack:set heroku-16
 heroku plugins:install heroku-container-registry
 docker login -u aygunaydin@gmail.com --password=$HEROKU_API_KEY registry.heroku.com
-heroku stack:set heroku-16
 heroku container:release web --app $HEROKU_APP_NAME
-
